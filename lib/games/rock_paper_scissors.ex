2f1a5fdb7afb080a17a20173c354b895088e2059
James Kent <107423859+jameskentdev@users.noreply.github.com>
2023-11-24 18:43:24 +0100

    Implemented the rock, paper & scissors game.

---
+++ lib/games/rock_paper_scissors.ex
@@ -0,0 +1,24 @@
+defmodule Games.RockPaperScissors do
+  def play() do
+    ai = Enum.random([:rock, :paper, :scissors])
+
+    guess =
+      IO.gets("Choose rock, paper, or scissors: ")
+      |> String.trim()
+      |> case do
+        "rock" -> :rock
+        "paper" -> :paper
+        "scissors" -> :scissors
+      end
+
+    cond do
+      ai == guess -> "It's a tie!"
+      beats?(guess, ai) -> "You win!"
+      beats?(ai, guess) -> "You lose!"
+    end
+  end
+
+  defp beats?(player1, player2) do
+    {player1, player2} in [{:rock, :scissors}, {:paper, :rock}, {:scissors, :paper}]
+  end
+end
